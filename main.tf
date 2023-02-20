bd586b2c351b680e49f9441dc7418f36c386d6bc
root <root@ip-172-31-65-83.ec2.internal>
2023-02-20 06:42:30 +0000

    Add Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.35"
+    }
+  }
+}
+
+provider "snowflake" {
+  role  = "SYSADMIN"
+  account = "HFB05743"
+  username = "SRINIVASTFDEMO"
+}
+
+resource "snowflake_database" "db" {
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "small"
+
+  auto_suspend = 60
+}
